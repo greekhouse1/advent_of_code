7d29bfc90cc5d20c2a0ac5a7610dac584b3af840
Brendon <greekhouse@gmail.com>
2023-12-07 21:26:38 -0500

    Swap day 6 and day 7

--- day06.py
+++ day06.py
@@ -1,84 +1,38 @@
-from collections import Counter
-from dataclasses import dataclass
-from enum import IntEnum
-from functools import total_ordering
-from pathlib import Path
+from typing import List
 
 
-FILE = Path() / "docs" / "day07.txt"
+TEST_TIMES = [7, 15, 30]
+TEST_DISTANCES = [9, 40, 200]
+
+REAL_TIMES = [47, 98, 66, 98]
+REAL_DISTANCES = [400, 1213, 1011, 1540]
+
+TIMES = REAL_TIMES
+DISTANCES = REAL_DISTANCES
+
+# ht + st = tt
+# d = ht*st = ht*(tt - ht)
 
 
-class HandType(IntEnum):
-    HIGH_CARD = 1
-    ONE_PAIR = 2
-    TWO_PAIR = 3
-    THREE_OF_A_KIND = 4
-    FULL_HOUSE = 5
-    FOUR_OF_A_KIND = 6
-    FIVE_OF_A_KIND = 7
+def race_distances(total_time: int) -> List[int]:
+    return [t * (total_time - t) for t in range(total_time + 1)]
 
 
-@dataclass
-@total_ordering
-class Hand:
-    order = "J23456789TQKA"
-    value: str
-    hand_type: HandType
+product = 1
 
-    def __init__(self, value: str, bet: int):
-        self.value = value
-        self.bet = bet
+for total_time, record_distance in zip(TIMES, DISTANCES):
+    distances = race_distances(total_time)
+    num_ways_to_win = len([x for x in distances if x > record_distance])
+    product *= num_ways_to_win
 
-        if "J" not in self.value:
-            self.hand_type = self.get_hand_type(Counter(self.value))
-        elif self.value == "JJJJJ":
-            self.hand_type = HandType.FIVE_OF_A_KIND
-        else:
-            possible_hand_types = []
-            non_joker = [x for x in self.value if x != "J"]
-            for x in non_joker:
-                new_value = self.value.replace("J", x)
-                possible_hand_types.append(self.get_hand_type(Counter(new_value)))
-                self.hand_type = max(possible_hand_types)
+print(f"1: {product}")
+
+TIMES = [int("".join(map(str, TIMES)))]
+DISTANCES = [int("".join(map(str, DISTANCES)))]
 
 
-    def get_hand_type(self, c: Counter):
-        if len(c) == 1:
-            return HandType.FIVE_OF_A_KIND
-        elif len(c) == 2:
-            if max(c.values()) == 4:
-                return HandType.FOUR_OF_A_KIND
-            else:
-                return HandType.FULL_HOUSE
-        elif len(c) == 3:
-            if max(c.values()) == 3:
-                return HandType.THREE_OF_A_KIND
-            else:
-                return HandType.TWO_PAIR
-        elif len(c) == 4:
-            return HandType.ONE_PAIR
-        else:
-            return HandType.HIGH_CARD
+for total_time, record_distance in zip(TIMES, DISTANCES):
+    distances = race_distances(total_time)
+    num_ways_to_win = len([x for x in distances if x > record_distance])
 
-    def to_tuple(self):
-        return (self.hand_type,) + tuple(self.order.index(x) for x in self.value)
-
-    def __eq__(self, value: "Hand") -> bool:
-        return self.to_tuple() == value.to_tuple()
-
-    def __lt__(self, value: "Hand") -> bool:
-        return self.to_tuple() < value.to_tuple()
-
-
-with open(FILE) as f:
-    hands = []
-    for line in f:
-        hand, bet = line.rstrip("\n").split()
-        hands.append(Hand(hand, int(bet)))
-
-hands.sort()
-winnings = 0
-for idx, hand in enumerate(hands):
-    winnings += (idx + 1) * hand.bet
-
-print(f"2: {winnings}")
+print(f"2: {num_ways_to_win}")
